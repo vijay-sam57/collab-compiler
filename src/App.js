2bfd1fe008902b214737ba473e80278b10e449cb
Vijay Sam <vijay203.vk@gmail.com>
2022-02-24 20:03:16 +0530

    Added Textarea and improved colors

--- src/App.js
+++ src/App.js
@@ -2,13 +2,30 @@ import "./App.css";
 
 function App() {
   return (
-    <nav>
-      <ul>
-        <li>Collab Compiler</li>
-        <li>Code</li>
-        <li>Projects</li>
-      </ul>
-    </nav>
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <a id="sitename" href="#">
+              Collab Compiler
+            </a>
+          </li>
+          <li>
+            <a href="#">Code</a>
+          </li>
+          <li>
+            <a href="#">Projects</a>
+          </li>
+        </ul>
+      </nav>
+      <div className="text">
+        <textarea
+          rows={27}
+          cols={64}
+          placeholder={"Enter your code here"}
+        ></textarea>
+      </div>
+    </div>
   );
 }
 
